77b43d20d287d160251ceb03cea80a118583d6fb
L YQ <2753079667@qq.com>
2019-01-21 10:15:16 +0000

    修复图片部署到heroku定时消失bug

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,7 +5,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
+  #storage :file
+  if Rails.env.development? #开发环境执行以下代码
+    storage :file           #图片储存到本地
+  else  #非开发环境，也就是部署到 heroku 等地方时，执行以下代码
+     storage :qiniu          #图片储存到七牛云
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
